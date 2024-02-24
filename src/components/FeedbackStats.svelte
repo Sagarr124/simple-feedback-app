833b0821515cd1705e4ee81ec2f9cded7b79e0ba
Sagar <sagarkph@gmail.com>
2024-02-24 20:09:42 +0500

    complete app

---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { FeedbackStore } from "../store.js";
+
+  $: count = $FeedbackStore.length;
+  $: average =
+    $FeedbackStore.reduce((acc, item) => acc + item.rating, 0) / count;
+</script>
+
+<div class="feedback-stats">
+  <h4>{count} Reviews</h4>
+  <h4>Ratings Average: {average}</h4>
+</div>
+
+<style>
+  .feedback-stats {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
